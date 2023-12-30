8662c3b3e46b9abaabb94dba86b548df7a9c3efc
FoulaSid <144328164+FoulaSid@users.noreply.github.com>
2023-12-30 22:54:16 +0200

    Update games.js

--- js/games.js
+++ js/games.js
@@ -7,49 +7,45 @@ document.getElementById("username").addEventListener("input", debounce(function(
 
 
 //Elegxos an to username uparxei mesw AJAX request
-function checkUsername(username)
-{
-  var xhr = new XMLHttpRequest();
-    xhr.open("POST", "../lib/register.php", true);
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            if (this.responseText == "exists") {
-                document.getElementById("input-error").style.color="red"
-            } 
-            else{
-            document.getElementById("input-error").style.color="#e4c473";
-            }
-          }
-        }
-   xhr.send("action=check_username&username=" + encodeURIComponent(username));
-
-}
-
-//Elegxos an to username  kai prin to submit  mesw AJAX request kai emfanish success pop up
-function checkBeforeSubmit(username) {
+function checkUsername(username) {
     var xhr = new XMLHttpRequest();
-    var username = document.getElementById("username").value;
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
     xhr.open("POST", "../lib/register.php", true);
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
     xhr.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            if (this.responseText == "exists") { //an to username uparxei , epistrefei h php exists
-                document.getElementById("input-error").style.color="red";
-            } else if(username.trim() !== "" && password.trim() !== "" && email.trim() !== "")  { //an einai ola sumplhrwmena 
-                openPopup(); // anoikse success pop up
-                document.getElementById("input-error").style.color="#e4c473";
-
-                
+            var response = JSON.parse(this.responseText);
+            if (response.exists) {
+                document.getElementById("input-error").innerHTML = "This username already exists!";
+            } else {
+                document.getElementById("input-error").innerHTML = "&nbsp";
             }
         }
     };
     xhr.send("action=check_username&username=" + encodeURIComponent(username));
 }
 
-
+//Elegxos gia username kai prin to submit  mesw AJAX request kai emfanish success pop up
+function checkBeforeSubmit(username) {
+    var xhr = new XMLHttpRequest();
+    var username = document.getElementById("username").value;
+    var email = document.getElementById("email").value;
+    var emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/; //an exei valei __@.__ sto mail
+    var password = document.getElementById("password").value;
+    xhr.open("POST", "../lib/register.php", true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var response = JSON.parse(this.responseText);
+            if (response.exists) {
+                document.getElementById("input-error").innerHTML = "This username already exists!";
+            } else if(username.trim() !== "" && password.trim() !== "" && emailRegex.test(email) ) {
+                openPopup(); // Open success popup
+                document.getElementById("input-error").innerHTML = "&nbsp";
+            }
+        }
+    };
+    xhr.send("action=check_username&username=" + encodeURIComponent(username));
+}
 
 
 function debounce(func, wait, immediate) {
@@ -73,8 +69,6 @@ function openPopup() {
     var popup = document.getElementById("popup");
     popup.classList.add("open-popup");
 }
-
-
 
 //Otan pathsei OK tha ginei to submit
 function submitForm() {
