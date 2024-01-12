c2ad5975cf211cdf53489640cac439d64c6a3c77
FoulaSid <144328164+FoulaSid@users.noreply.github.com>
2024-01-12 04:18:25 +0200

    Update dbconnect.php

--- lib/dbconnect.php
+++ lib/dbconnect.php
@@ -1,16 +1,28 @@
 <?php
-$host = '127.0.0.1'; 
-$port = '3333'; 
-$db   = 'GAMES'; 
-require_once "db_upass.php";
+$host='localhost';
+$db ='Games';
+require_once 'db_upass.php';
+
+
 $user=$DB_USER;
 $pass=$DB_PASS;
 
-try {
-    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$db", $user, $pass);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+if(gethostname()=='users.iee.ihu.gr'){
+    $mysqli = new mysqli($host,$user,$pass,$db,null,' /home/student/iee/2019/iee2019149/mysql/run/mysql.sock');
+    $conn=$mysqli;
+
+} else {
+    $pass = null;
+    $mysqli = new mysqli($host,$user,$pass,$db);
+    $conn=$mysqli;
 }
-catch(PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
+
+if($mysqli->connect_errno){
+    echo "Failed to connect to MySQL: (" . 
+    $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
+
+
+
+
 ?>
